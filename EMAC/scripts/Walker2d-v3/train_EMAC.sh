ba83f5831216a3b69a47ccd0676e4b338df7bfa2
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-08-18 00:17:50 +0900

    update

--- EMAC/scripts/Walker2d-v3/train_EMAC.sh
+++ EMAC/scripts/Walker2d-v3/train_EMAC.sh
@@ -1,5 +1,5 @@
-python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 1000001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 0
-python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 1000001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 1
-python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 1000001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 2
-python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 1000001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 3
-python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 1000001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 4
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 0
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 1
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 2
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 3
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 4
