8ebe20155c2d04bac22140131c265e2fe8cb383b
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-08-20 20:10:06 +0900

    update

--- NECSA/tianshou/data/__init__.py
+++ NECSA/tianshou/data/__init__.py
@@ -16,6 +16,7 @@ from tianshou.data.buffer.vecbuf import (
 )
 from tianshou.data.buffer.cached import CachedReplayBuffer
 from tianshou.data.collector import Collector, AsyncCollector
+from tianshou.data.necsa_collector import NECSA_Collector
 
 __all__ = [
     "Batch",
