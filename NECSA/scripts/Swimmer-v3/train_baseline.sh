bd738114a10c90fdb59b7fdd5029347a64f02370
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-08-25 16:43:31 +0900

    update

--- NECSA/scripts/Swimmer-v3/train_baseline.sh
+++ NECSA/scripts/Swimmer-v3/train_baseline.sh
@@ -1,33 +1,22 @@
- python ddpg.py --task Swimmer-v3 --epoch 400
+python  ddpg.py --task Swimmer-v3 --epoch 400
 pkill -f ddpg
- python ddpg.py --task Swimmer-v3 --epoch 400
+python  ddpg.py --task Swimmer-v3 --epoch 400
 pkill -f ddpg
- python ddpg.py --task Swimmer-v3 --epoch 400
+python  ddpg.py --task Swimmer-v3 --epoch 400
 pkill -f ddpg
- python ddpg.py --task Swimmer-v3 --epoch 400
+python  ddpg.py --task Swimmer-v3 --epoch 400
 pkill -f ddpg
- python ddpg.py --task Swimmer-v3 --epoch 400
+python  ddpg.py --task Swimmer-v3 --epoch 400
 pkill -f ddpg
 
 
- python ppo.py --task Swimmer-v3 --epoch 400
-pkill -f ppo
- python ppo.py --task Swimmer-v3 --epoch 400
-pkill -f ppo
- python ppo.py --task Swimmer-v3 --epoch 400
-pkill -f ppo
- python ppo.py --task Swimmer-v3 --epoch 400
-pkill -f ppo
- python ppo.py --task Swimmer-v3 --epoch 400
-pkill -f ppo
-
- python td3.py --task Swimmer-v3 --epoch 400
+python  td3.py --task Swimmer-v3 --epoch 400
 pkill -f td3
- python td3.py --task Swimmer-v3 --epoch 400
+python  td3.py --task Swimmer-v3 --epoch 400
 pkill -f td3
- python td3.py --task Swimmer-v3 --epoch 400
+python  td3.py --task Swimmer-v3 --epoch 400
 pkill -f td3
- python td3.py --task Swimmer-v3 --epoch 400
+python  td3.py --task Swimmer-v3 --epoch 400
 pkill -f td3
- python td3.py --task Swimmer-v3 --epoch 400
+python  td3.py --task Swimmer-v3 --epoch 400
 pkill -f td3
--- NECSA/scripts/Walker2d-v3/train_baseline.sh
+++ NECSA/scripts/Walker2d-v3/train_baseline.sh
@@ -10,16 +10,6 @@ python ddpg.py --task Walker2d-v3 --epoch 400
 pkill -f ddpg
 
 
-python ppo.py --task Walker2d-v3 --epoch 400
-pkill -f ppo
-python ppo.py --task Walker2d-v3 --epoch 400
-pkill -f ppo
-python ppo.py --task Walker2d-v3 --epoch 400
-pkill -f ppo
-python ppo.py --task Walker2d-v3 --epoch 400
-pkill -f ppo
-python ppo.py --task Walker2d-v3 --epoch 400
-pkill -f ppo
 
 python td3.py --task Walker2d-v3 --epoch 400
 pkill -f td3
