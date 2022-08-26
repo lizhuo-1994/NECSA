3a24e5174138cdee33f17e9bb12fa5fc2b69b35d
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-08-26 09:35:57 +0900

    update

--- NECSA/scripts/Humanoid-v3/train_NECSA_TD3.sh
+++ NECSA/scripts/Humanoid-v3/train_NECSA_TD3.sh
@@ -1,33 +1,33 @@
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 1 --grid_num 5 --decay 1.0 --mode state_action
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 1 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 1 --grid_num 5 --decay 1.0 --mode state_action
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 1 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 1 --grid_num 5 --decay 1.0 --mode state_action
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 1 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 1 --grid_num 5 --decay 1.0 --mode state_action
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 1 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 1 --grid_num 5 --decay 1.0 --mode state_action
-pkill -f necsa
-
-
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 2 --grid_num 5 --decay 1.0 --mode state_action
-pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 2 --grid_num 5 --decay 1.0 --mode state_action
-pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 2 --grid_num 5 --decay 1.0 --mode state_action
-pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 2 --grid_num 5 --decay 1.0 --mode state_action
-pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 2 --grid_num 5 --decay 1.0 --mode state_action
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 1 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
 
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 3 --grid_num 5 --decay 1.0 --mode state_action
+
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 2 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 3 --grid_num 5 --decay 1.0 --mode state_action
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 2 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 3 --grid_num 5 --decay 1.0 --mode state_action
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 2 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 3 --grid_num 5 --decay 1.0 --mode state_action
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 2 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
-python necsa_td3.py --task Hopper-v3  --epoch 400 --order 3 --grid_num 5 --decay 1.0 --mode state_action
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 2 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
+pkill -f necsa
+
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 3 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
+pkill -f necsa
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 3 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
+pkill -f necsa
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 3 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
+pkill -f necsa
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 3 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
+pkill -f necsa
+python necsa_td3.py --task Humanoid-v3  --epoch 500 --order 3 --grid_num 8 --state_dim 25 --state_min -6 --state_max 6 --decay 0.2 --mode state_action --reduction
 pkill -f necsa
