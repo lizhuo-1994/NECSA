c8a87112090a8f6a2c0f11703ecd00674aaf1a6b
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-11-18 18:45:30 +0900

    update

--- scripts/Alien/train_NECSA.sh
+++ scripts/Alien/train_NECSA.sh
@@ -1,35 +1,35 @@
-python necsa_dqn.py --task Alien-v5  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
+python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
 killall -9 python
-python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
+python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
 killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
 # killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
 # killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
 # killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
-# killall -9 python
-
-
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
-# killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
-# killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
-# killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
-# killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 1 --epsilon 0.1 --mode hidden --reduction
 # killall -9 python
 
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
+
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
 # killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
 # killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
 # killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
 # killall -9 python
-# python necsa_rainbow.py --task Alien-v5  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 2 --epsilon 0.1 --mode hidden --reduction
+# killall -9 python
+
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
+# killall -9 python
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
+# killall -9 python
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
+# killall -9 python
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
+# killall -9 python
+# python necsa_rainbow.py --task AlienNoFrameskip-v4  --epoch 1000 --step 3 --epsilon 0.1 --mode hidden --reduction
 # killall -9 python
---
+++ scripts/Alien/train_baseline.sh
@@ -0,0 +1,21 @@
+python dqn.py --task AlienNoFrameskip-v4 --epoch 1000
+killall -9 python
+python dqn.py --task AlienNoFrameskip-v4 --epoch 1000
+killall -9 python
+python dqn.py --task AlienNoFrameskip-v4 --epoch 1000
+killall -9 python
+
+
+python rainbow.py --task AlienNoFrameskip-v4 --epoch 1000
+killall -9 python
+python rainbow.py --task AlienNoFrameskip-v4 --epoch 1000
+killall -9 python
+python rainbow.py --task AlienNoFrameskip-v4 --epoch 1000
+killall -9 python
+
+python ppo.py --task AlienNoFrameskip-v4 --epoch 1000
+killall -9 python
+python ppo.py --task AlienNoFrameskip-v4 --epoch 1000
+killall -9 python
+python ppo.py --task AlienNoFrameskip-v4 --epoch 1000
+killall -9 python
