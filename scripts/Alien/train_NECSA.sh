eb118939de1ac4cb76fd7f351881026a15090f6c
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-11-22 01:56:04 +0900

    update

--- scripts/Alien/train_NECSA.sh
+++ scripts/Alien/train_NECSA.sh
@@ -7,18 +7,24 @@
 
 
 
-python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 2 --epsilon 0.5 --mode hidden --reduction
-killall -9 python
-python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 2 --epsilon 0.5 --mode hidden --reduction
-killall -9 python
+# python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 2 --epsilon 0.5 --mode hidden --reduction
+# killall -9 python
+# python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 2 --epsilon 0.5 --mode hidden --reduction
+# killall -9 python
+# python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 2 --epsilon 0.5 --mode hidden --reduction
+# killall -9 python
 
 
-python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 3 --epsilon 0.5 --mode hidden --reduction
-killall -9 python
-python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 3 --epsilon 0.5 --mode hidden --reduction
-killall -9 python
+# python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 3 --epsilon 0.5 --mode hidden --reduction
+# killall -9 python
+# python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 3 --epsilon 0.5 --mode hidden --reduction
+# killall -9 python
+# python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 3 --epsilon 0.5 --mode hidden --reduction
+# killall -9 python
 
 
+python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 3 --epsilon 1.0 --mode hidden --reduction
+killall -9 python
 python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 3 --epsilon 1.0 --mode hidden --reduction
 killall -9 python
 python necsa_dqn.py --task AlienNoFrameskip-v4  --epoch 500 --step 3 --epsilon 1.0 --mode hidden --reduction
---
+++ scripts/Breakout/train_baseline.sh
@@ -0,0 +1,21 @@
+# python dqn.py --task BreakoutNoFrameskip-v4 --epoch 500
+# killall -9 python
+# python dqn.py --task BreakoutNoFrameskip-v4 --epoch 500
+# killall -9 python
+# python dqn.py --task BreakoutNoFrameskip-v4 --epoch 500
+# killall -9 python
+
+
+python rainbow.py --task BreakoutNoFrameskip-v4 --epoch 500
+killall -9 python
+python rainbow.py --task BreakoutNoFrameskip-v4 --epoch 500
+killall -9 python
+python rainbow.py --task BreakoutNoFrameskip-v4 --epoch 500
+killall -9 python
+
+python ppo.py --task BreakoutNoFrameskip-v4 --epoch 500
+killall -9 python
+python ppo.py --task BreakoutNoFrameskip-v4 --epoch 500
+killall -9 python
+python ppo.py --task BreakoutNoFrameskip-v4 --epoch 500
+killall -9 python
