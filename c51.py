b012d0e0ab7129ef44319f6795b3daa5df21fbda
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-11-25 14:24:08 +0900

    update

--- c51.py
+++ c51.py
@@ -5,46 +5,39 @@ import pprint
 
 import numpy as np
 import torch
-from atari_network import DQN
+from atari_network import C51
 from atari_wrapper import make_atari_env
-from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.tensorboard import SummaryWriter
 
 from tianshou.data import Collector, VectorReplayBuffer
-from tianshou.policy import ICMPolicy, PPOPolicy
-from tianshou.trainer import onpolicy_trainer
+from tianshou.policy import C51Policy
+from tianshou.trainer import offpolicy_trainer
 from tianshou.utils import TensorboardLogger, WandbLogger
-from tianshou.utils.net.common import ActorCritic
-from tianshou.utils.net.discrete import Actor, Critic, IntrinsicCuriosityModule
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--task", type=str, default="PongNoFrameskip-v4")
-    parser.add_argument("--seed", type=int, default=4213)
+    parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--scale-obs", type=int, default=0)
+    parser.add_argument("--eps-test", type=float, default=0.005)
+    parser.add_argument("--eps-train", type=float, default=1.)
+    parser.add_argument("--eps-train-final", type=float, default=0.05)
     parser.add_argument("--buffer-size", type=int, default=100000)
-    parser.add_argument("--lr", type=float, default=5e-5)
+    parser.add_argument("--lr", type=float, default=0.0001)
     parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--num-atoms", type=int, default=51)
+    parser.add_argument("--v-min", type=float, default=-10.)
+    parser.add_argument("--v-max", type=float, default=10.)
+    parser.add_argument("--n-step", type=int, default=3)
+    parser.add_argument("--target-update-freq", type=int, default=500)
     parser.add_argument("--epoch", type=int, default=100)
     parser.add_argument("--step-per-epoch", type=int, default=100000)
-    parser.add_argument("--step-per-collect", type=int, default=1000)
-    parser.add_argument("--repeat-per-collect", type=int, default=4)
-    parser.add_argument("--batch-size", type=int, default=256)
-    parser.add_argument("--hidden-size", type=int, default=512)
+    parser.add_argument("--step-per-collect", type=int, default=10)
+    parser.add_argument("--update-per-step", type=float, default=0.1)
+    parser.add_argument("--batch-size", type=int, default=32)
     parser.add_argument("--training-num", type=int, default=10)
     parser.add_argument("--test-num", type=int, default=10)
-    parser.add_argument("--rew-norm", type=int, default=False)
-    parser.add_argument("--vf-coef", type=float, default=0.5)
-    parser.add_argument("--ent-coef", type=float, default=0.01)
-    parser.add_argument("--gae-lambda", type=float, default=0.95)
-    parser.add_argument("--lr-decay", type=int, default=True)
-    parser.add_argument("--max-grad-norm", type=float, default=0.5)
-    parser.add_argument("--eps-clip", type=float, default=0.2)
-    parser.add_argument("--dual-clip", type=float, default=None)
-    parser.add_argument("--value-clip", type=int, default=0)
-    parser.add_argument("--norm-adv", type=int, default=1)
-    parser.add_argument("--recompute-adv", type=int, default=0)
     parser.add_argument("--logdir", type=str, default="log")
     parser.add_argument("--render", type=float, default=0.)
     parser.add_argument(
@@ -67,28 +60,10 @@ def get_args():
         help="watch the play of pre-trained policy only"
     )
     parser.add_argument("--save-buffer-name", type=str, default=None)
-    parser.add_argument(
-        "--icm-lr-scale",
-        type=float,
-        default=0.,
-        help="use intrinsic curiosity module with this lr scale"
-    )
-    parser.add_argument(
-        "--icm-reward-scale",
-        type=float,
-        default=0.01,
-        help="scaling factor for intrinsic curiosity reward"
-    )
-    parser.add_argument(
-        "--icm-forward-loss-weight",
-        type=float,
-        default=0.2,
-        help="weight for the forward model loss in ICM"
-    )
     return parser.parse_args()
 
 
-def test_ppo(args=get_args()):
+def test_c51(args=get_args()):
     env, train_envs, test_envs = make_atari_env(
         args.task,
         args.seed,
@@ -106,70 +81,19 @@ def test_ppo(args=get_args()):
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     # define model
-    net = DQN(
-        *args.state_shape,
-        args.action_shape,
-        device=args.device,
-        features_only=True,
-        output_dim=args.hidden_size
-    )
-    actor = Actor(net, args.action_shape, device=args.device, softmax_output=False)
-    critic = Critic(net, device=args.device)
-    optim = torch.optim.Adam(ActorCritic(actor, critic).parameters(), lr=args.lr)
-
-    lr_scheduler = None
-    if args.lr_decay:
-        # decay learning rate to 0 linearly
-        max_update_num = np.ceil(
-            args.step_per_epoch / args.step_per_collect
-        ) * args.epoch
-
-        lr_scheduler = LambdaLR(
-            optim, lr_lambda=lambda epoch: 1 - epoch / max_update_num
-        )
-
+    net = C51(*args.state_shape, args.action_shape, args.num_atoms, args.device)
+    optim = torch.optim.Adam(net.parameters(), lr=args.lr)
     # define policy
-    def dist(p):
-        return torch.distributions.Categorical(logits=p)
-
-    policy = PPOPolicy(
-        actor,
-        critic,
+    policy = C51Policy(
+        net,
         optim,
-        dist,
-        discount_factor=args.gamma,
-        gae_lambda=args.gae_lambda,
-        max_grad_norm=args.max_grad_norm,
-        vf_coef=args.vf_coef,
-        ent_coef=args.ent_coef,
-        reward_normalization=args.rew_norm,
-        action_scaling=False,
-        lr_scheduler=lr_scheduler,
-        action_space=env.action_space,
-        eps_clip=args.eps_clip,
-        value_clip=args.value_clip,
-        dual_clip=args.dual_clip,
-        advantage_normalization=args.norm_adv,
-        recompute_advantage=args.recompute_adv,
+        args.gamma,
+        args.num_atoms,
+        args.v_min,
+        args.v_max,
+        args.n_step,
+        target_update_freq=args.target_update_freq
     ).to(args.device)
-    if args.icm_lr_scale > 0:
-        feature_net = DQN(
-            *args.state_shape, args.action_shape, args.device, features_only=True
-        )
-        action_dim = np.prod(args.action_shape)
-        feature_dim = feature_net.output_dim
-        icm_net = IntrinsicCuriosityModule(
-            feature_net.net,
-            feature_dim,
-            action_dim,
-            hidden_sizes=[args.hidden_size],
-            device=args.device,
-        )
-        icm_optim = torch.optim.Adam(icm_net.parameters(), lr=args.lr)
-        policy = ICMPolicy(
-            policy, icm_net, icm_optim, args.icm_lr_scale, args.icm_reward_scale,
-            args.icm_forward_loss_weight
-        ).to(args.device)
     # load a previous policy
     if args.resume_path:
         policy.load_state_dict(torch.load(args.resume_path, map_location=args.device))
@@ -181,17 +105,15 @@ def test_ppo(args=get_args()):
         buffer_num=len(train_envs),
         ignore_obs_next=True,
         save_only_last_obs=True,
-        stack_num=args.frames_stack,
+        stack_num=args.frames_stack
     )
     # collector
     train_collector = Collector(policy, train_envs, buffer, exploration_noise=True)
     test_collector = Collector(policy, test_envs, exploration_noise=True)
 
-    args.algo_name = 'ppo'
-
     # log
     now = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
-    args.algo_name = "ppo_icm" if args.icm_lr_scale > 0 else "ppo"
+    args.algo_name = "c51"
     log_name = os.path.join(args.task, args.algo_name, str(args.seed), now)
     log_path = os.path.join(args.logdir, log_name)
 
@@ -222,16 +144,25 @@ def test_ppo(args=get_args()):
         else:
             return False
 
-    def save_checkpoint_fn(epoch, env_step, gradient_step):
-        # see also: https://pytorch.org/tutorials/beginner/saving_loading_models.html
-        ckpt_path = os.path.join(log_path, f"checkpoint_{epoch}.pth")
-        torch.save({"model": policy.state_dict()}, ckpt_path)
-        return ckpt_path
+    def train_fn(epoch, env_step):
+        # nature DQN setting, linear decay in the first 1M steps
+        if env_step <= 1e6:
+            eps = args.eps_train - env_step / 1e6 * \
+                (args.eps_train - args.eps_train_final)
+        else:
+            eps = args.eps_train_final
+        policy.set_eps(eps)
+        if env_step % 1000 == 0:
+            logger.write("train/env_step", env_step, {"train/eps": eps})
+
+    def test_fn(epoch, env_step):
+        policy.set_eps(args.eps_test)
 
     # watch agent's performance
     def watch():
         print("Setup test envs ...")
         policy.eval()
+        policy.set_eps(args.eps_test)
         test_envs.seed(args.seed)
         if args.save_buffer_name:
             print(f"Generate buffer with size {args.buffer_size}")
@@ -240,7 +171,7 @@ def test_ppo(args=get_args()):
                 buffer_num=len(test_envs),
                 ignore_obs_next=True,
                 save_only_last_obs=True,
-                stack_num=args.frames_stack,
+                stack_num=args.frames_stack
             )
             collector = Collector(policy, test_envs, buffer, exploration_noise=True)
             result = collector.collect(n_step=args.buffer_size)
@@ -263,22 +194,22 @@ def test_ppo(args=get_args()):
     # test train_collector and start filling replay buffer
     train_collector.collect(n_step=args.batch_size * args.training_num)
     # trainer
-    result = onpolicy_trainer(
+    result = offpolicy_trainer(
         policy,
         train_collector,
         test_collector,
         args.epoch,
         args.step_per_epoch,
-        args.repeat_per_collect,
+        args.step_per_collect,
         args.test_num,
         args.batch_size,
-        step_per_collect=args.step_per_collect,
+        train_fn=train_fn,
+        test_fn=test_fn,
         stop_fn=stop_fn,
         save_best_fn=save_best_fn,
         logger=logger,
+        update_per_step=args.update_per_step,
         test_in_train=False,
-        resume_from_log=args.resume_id is not None,
-        save_checkpoint_fn=save_checkpoint_fn,
     )
 
     pprint.pprint(result)
@@ -294,5 +225,6 @@ def test_ppo(args=get_args()):
     with open(reward_save_path, 'w') as f:
         json.dump(test_collector.policy_eval_results, f)
 
+
 if __name__ == "__main__":
-    test_ppo(get_args())
+    test_c51(get_args())
