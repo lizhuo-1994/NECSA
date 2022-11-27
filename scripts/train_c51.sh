9291d3b541c015b4727082150722e2bc53ab4b8e
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-11-27 22:00:31 +0900

    update

--- scripts/train_c51.sh
+++ scripts/train_c51.sh
@@ -1,9 +1,9 @@
-# python c51.py --task AlienNoFrameskip-v4 --epoch 500
-# killall -9 python
-# python c51.py --task AlienNoFrameskip-v4 --epoch 500
-# killall -9 python
-# python c51.py --task AlienNoFrameskip-v4 --epoch 500
-# killall -9 python
+python c51.py --task AlienNoFrameskip-v4 --epoch 500
+killall -9 python
+python c51.py --task AlienNoFrameskip-v4 --epoch 500
+killall -9 python
+python c51.py --task AlienNoFrameskip-v4 --epoch 500
+killall -9 python
 
 python c51.py --task BreakoutNoFrameskip-v4 --epoch 500
 killall -9 python
@@ -35,9 +35,9 @@ python c51.py --task SpaceInvadersNoFrameskip-v4 --epoch 500
 killall -9 python
 
 
-# python c51.py --task QbertNoFrameskip-v4 --epoch 500
-# killall -9 python
-# python c51.py --task QbertNoFrameskip-v4 --epoch 500
-# killall -9 python
-# python c51.py --task QbertNoFrameskip-v4 --epoch 500
-# killall -9 python
+python c51.py --task QbertNoFrameskip-v4 --epoch 500
+killall -9 python
+python c51.py --task QbertNoFrameskip-v4 --epoch 500
+killall -9 python
+python c51.py --task QbertNoFrameskip-v4 --epoch 500
+killall -9 python
